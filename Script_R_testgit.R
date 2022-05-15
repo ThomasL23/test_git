466c6c8f3d2a62b6d15876ecbb690f43f656f9be
Lerosier Thomas <thomaslerosier@live.fr>
2022-05-15 12:32:24 +0000

    deuxièmecommit avec ajout sur Marie (2e tentative)

--- Script_R_testgit.R
+++ Script_R_testgit.R
@@ -2,3 +2,12 @@
 
 print("c'est trop bon la ratatouille")
 
+# Je continue mon script après le premier commit. 
+print("Enfin, tout le monde n'aime pas")
+
+XXX <- "Thomas"
+print(XXX)
+
+#Je poursuit mon script avant de lancer un 2e commit
+XXX <- "Marie"
+print("THOMAS AIME MARIE")
